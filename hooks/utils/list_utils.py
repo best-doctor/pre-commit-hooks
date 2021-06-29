83b1fc921e8df675d52e95a5dc96ee902d636907
sidorov-as <ereon.dev@gmail.com>
2021-06-29 11:54:33 +0300

    ENG-1175: bump mypy version to 0.812

--- hooks/utils/list_utils.py
+++ hooks/utils/list_utils.py
@@ -1,15 +1,23 @@
 from __future__ import annotations
 
-from typing import Iterable, List, Optional, TypeVar, Union
-
-T = TypeVar('T')
+import typing
 
 
-def flat(some_list: Iterable[Iterable]) -> List:
+class SupportsLessThan(typing.Protocol):
+    def __lt__(self, __other: typing.Any) -> bool:
+        ...
+
+
+T = typing.TypeVar('T', bound=SupportsLessThan)
+
+
+def flat(some_list: typing.Iterable[typing.Iterable]) -> typing.List:
     return [item for sublist in some_list for item in sublist]
 
 
-def max_with_default(items: Iterable[T], default: Optional[T] = None) -> Union[T, int]:
+def max_with_default(
+    items: typing.Iterable[T], default: typing.Optional[T] = None
+) -> typing.Union[T, int]:
     default = default or 0
     items = list(items)
     if not items and default is not None:
